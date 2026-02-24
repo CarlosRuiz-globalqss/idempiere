b0a828eb9bf4026aebb3615ccff92d9596254dee
pultzlucas <pultzlucas@gmail.com>
2026-02-24 17:53:55 -0300

    IDEMPIERE-6874 Copy from a Product - Process Adjustment

---
+++ migration/iD12/oracle/202602241739_IDEMPIERE-6874.sql
@@ -0,0 +1,9 @@
+-- IDEMPIERE-6874 Copy from a Product - Process Adjustment
+SELECT register_migration_script('202602241739_IDEMPIERE-6874.sql') FROM dual;
+
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- Feb 23, 2026, 2:06:26 PM CET
+UPDATE AD_Column SET IsAllowCopy='N', SeqNoSelection=10,Updated=TO_TIMESTAMP('2026-02-23 14:06:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53321
+;
